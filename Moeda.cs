2a734f62162784b7fc9a0f23eeddf8074302ae59
Gabriel Sanga <gabriel.sanga.fef@gmail.com>
2023-02-05 10:42:26 -0300

    Desfeito

--- Moeda.cs
+++ Moeda.cs
@@ -8,6 +8,8 @@ namespace Projeto
     {
         public static void Aplication()
         {
+            Console.Clear();
+
             decimal valor = 10322.95m;
 
             //Formats de número: C, E04, G, F, N, P
